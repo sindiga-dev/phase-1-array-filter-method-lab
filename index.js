89a23ca9264886e0a3371cc289a53b772257fa8f
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-08-23 11:31:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+function findMatching(drivers, name){
+        return drivers
+        .filter(function(driver){
+            return driver.toLowerCase().includes(name.toLowerCase());
+        })
+        .sort()
+}
+function fuzzyMatch(drivers, name){
+    return drivers
+    .filter(function(driver){
+        return driver.toLowerCase().startsWith(name.toLowerCase());
+    })
+    .sort()
+}
+function matchName(drivers, name){
+    return drivers
+    .filter(function(driver){
+        return driver.name.toLowerCase()===name.toLowerCase();
+    })
+    .sort()
+}
